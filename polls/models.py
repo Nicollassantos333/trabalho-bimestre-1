2fc6e9df32cc787ff3e17e8b101ac45c9e70943f
Nicollas Santos <nicollassantos660@gmail.com>
2026-04-08 23:17:30 -0300

    Bug estético no admin resolvido

--- polls/models.py
+++ polls/models.py
@@ -6,6 +6,5 @@ class pergunta(models.Model):
     nome = models.CharField(verbose_name="Insira seu nome", max_length=20)
     email = models.EmailField(verbose_name="Insira seu endereço de Email", max_length=250)
     senha = models.CharField(verbose_name="Insira sua senha", max_length=20)
-
-def __str__(self):
-        return self.nome
+    def __str__(self):
+            return self.nome
